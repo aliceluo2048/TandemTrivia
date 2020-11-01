86f3aa65810af4735909f9ee0fb8e965fb03062b
Alice Luo <squeeshed@gmail.com>
2020-11-01 12:21:40 -0600

    Util functions

--- TandemTrivia/Program.cs
+++ TandemTrivia/Program.cs
@@ -9,9 +9,9 @@ namespace TandemTrivia
         public static void Main(string[] args)
         {
             var questions = TriviaQuestion.LoadFromFile(AppDomain.CurrentDomain.BaseDirectory + "\\" + "Apprentice_TandemFor400_Data.json");
-            Shuffle(questions);
+            Util.Shuffle(questions);
             var round = questions.Take(10).ToList();
-            int score = 0; 
+            int score = 0;
 
             foreach (TriviaQuestion question in round)
             {
@@ -19,14 +19,14 @@ namespace TandemTrivia
                 var multipleChoiceAnswers = new List<string>();
                 multipleChoiceAnswers.AddRange(question.Incorrect);
                 multipleChoiceAnswers.Add(question.Correct);
-                Shuffle(multipleChoiceAnswers);
-         
+                Util.Shuffle(multipleChoiceAnswers);
+
                 for (int i = 0; i < multipleChoiceAnswers.Count; i++)
                 {
                     Console.WriteLine($"{i + 1}" + " " + $"{multipleChoiceAnswers[i]}");
                 }
 
-                var userAnswer = ReadAnswer();
+                var userAnswer = Util.ReadAnswer();
 
                 if (multipleChoiceAnswers[int.Parse(userAnswer) - 1] == question.Correct)
                 {
@@ -44,38 +44,6 @@ namespace TandemTrivia
             }
 
             Console.WriteLine($"You have finished a round of trivia. Your score is {score} out of 10");
-        }
-
-        static bool IsValidAnswer(string userAnswer)
-        {
-            int number;
-            bool success = Int32.TryParse(userAnswer, out number);
-            return success && number >= 1 && number <= 4;
-        }
-
-        static string ReadAnswer()
-        {
-            var userAnswer = Console.ReadLine();
-            while (!IsValidAnswer(userAnswer))
-            {
-                Console.WriteLine("Please enter a valid answer. It should be between 1 - 4");
-                userAnswer = Console.ReadLine();
-            }
-
-            return userAnswer;
-        }
-
-        // Fisher-Yates shuffle code from https://developerslogblog.wordpress.com/2020/02/04/how-to-shuffle-an-array/
-        static void Shuffle<T>(List<T> list)
-        {
-            var random = new Random();
-            for (int i = list.Count - 1; i > 0; i--)
-            {
-                int randomIndex = random.Next(0, i + 1);
-                T temp = list[i];
-                list[i] = list[randomIndex];
-                list[randomIndex] = temp;
-            }
         }
     }
 }
---
+++ TandemTrivia/Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TandemTrivia
+{
+    public static class Util
+    {
+        public static bool IsValidAnswer(string userAnswer)
+        {
+            int number;
+            bool success = Int32.TryParse(userAnswer, out number);
+            return success && number >= 1 && number <= 4;
+        }
+
+        public static string ReadAnswer()
+        {
+            var userAnswer = Console.ReadLine();
+            while (!IsValidAnswer(userAnswer))
+            {
+                Console.WriteLine("Please enter a valid answer. It should be between 1 - 4");
+                userAnswer = Console.ReadLine();
+            }
+
+            return userAnswer;
+        }
+
+        // Fisher-Yates shuffle code from https://developerslogblog.wordpress.com/2020/02/04/how-to-shuffle-an-array/
+        public static void Shuffle<T>(List<T> list)
+        {
+            var random = new Random();
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int randomIndex = random.Next(0, i + 1);
+                T temp = list[i];
+                list[i] = list[randomIndex];
+                list[randomIndex] = temp;
+            }
+        }
+    }
+}
